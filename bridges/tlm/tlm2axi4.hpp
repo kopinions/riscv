3d7e1a457477ca50c6439f3e185a4e7afcbe5df8
neo <sjkyspa@gmail.com>
2020-07-24 00:34:30 +0800

    feat: should able to write bus

--- bridges/tlm/tlm2axi4.hpp
+++ bridges/tlm/tlm2axi4.hpp
@@ -20,8 +20,8 @@ class tlm2axi4 : public sc_core::sc_module {
   tlm2axi4(const sc_core::sc_module_name& name) : sc_module(name) {
     m_target.register_b_transport(this, &tlm2axi4<ADDR_WIDTH, DATA_WIDTH>::b_transport);
     SC_THREAD(read_address_phase)
-    SC_THREAD(write_address_phase)
     SC_THREAD(read_response_phase)
+    SC_THREAD(write_address_phase)
     SC_THREAD(write_data_phase)
     SC_THREAD(write_response_phase)
   }
@@ -40,9 +40,9 @@ class tlm2axi4 : public sc_core::sc_module {
   };
 
   sc_core::sc_fifo<transaction*> m_r_transaction_fifo;
+  sc_core::sc_fifo<transaction*> m_r_response;
   sc_core::sc_fifo<transaction*> m_w_transaction_fifo;
   sc_core::sc_fifo<transaction*> m_w_data_fifo;
-  sc_core::sc_fifo<transaction*> m_r_response;
   sc_core::sc_fifo<transaction*> m_w_response;
 
   sc_core::sc_mutex m_mutex;
@@ -65,6 +65,7 @@ class tlm2axi4 : public sc_core::sc_module {
       transaction* tx = m_r_transaction_fifo.read();
       std::cout << "read_address_phase" << std::endl;
       m_r_response.write(tx);
+      std::cout << "read_address_write" << std::endl;
     }
   }
 
@@ -79,15 +80,30 @@ class tlm2axi4 : public sc_core::sc_module {
   [[noreturn]] void read_response_phase() {
     while (true) {
       transaction* tx = NULL;
-      std::cout << "read_response_phase" << std::endl;
+
       tx = m_r_response.read();
+
+      std::cout << "read_response_phase" << std::endl;
       std::cout << "done" << std::endl;
       tx->done().notify();
     }
   }
 
-  void write_data_phase() {}
+  [[noreturn]] void write_data_phase() {
+    while (true) {
+      transaction* tx = m_w_data_fifo.read();
+      m_w_response.write(tx);
+      std::cout << "write_data_phase" << std::endl;
+    }
+  }
 
-  void write_response_phase() {}
+  [[noreturn]] void write_response_phase() {
+    while (true) {
+      transaction* tx;
+      tx = m_w_response.read();
+      tx->done().notify();
+      std::cout << "done" << std::endl;
+    }
+  }
 };
 #endif  // TLM2AXI4_HPP
