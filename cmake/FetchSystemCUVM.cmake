89a1498f05ceece20cf85e0f616c753efb028a67
neo <rising.lambda@gmail.com>
2022-07-04 23:17:02 +0800

    build: uvm compile compatable with macos

--- cmake/FetchSystemCUVM.cmake
+++ cmake/FetchSystemCUVM.cmake
@@ -2,10 +2,10 @@ include(ExternalProject)
 include(GNUInstallDirs)
 ExternalProject_Add(uvm-systemc-build
         URL https://github.com/kopinions/uvm-systemc/archive/refs/heads/main.zip
-        CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env CXXFLAGS=-std=c++17 SYSTEMC_CFLAGS=-std=c++17
-        LD_LIBRARY_PATH=$<TARGET_PROPERTY:systemc,PREFIX>/lib64:${LD_LIBRARY_PATH} <SOURCE_DIR>/configure --enable-shared
+        CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env CXXFLAGS=-std=c++17
+        LD_LIBRARY_PATH=$<TARGET_PROPERTY:systemc,PREFIX>/${CMAKE_INSTALL_LIBDIR}:${LD_LIBRARY_PATH} <SOURCE_DIR>/configure --enable-shared
         --with-systemc=$<TARGET_PROPERTY:systemc,PREFIX> --prefix=<INSTALL_DIR>
-        --with-arch-suffix=64 && make -j$(nproc) && make -j$(nproc) install
+        --with-arch-suffix=$<IF:$<STREQUAL:${CMAKE_INSTALL_LIBDIR},lib>,,64>  && make -j$(nproc) && make -j$(nproc) install
         )
 ExternalProject_Get_Property(uvm-systemc-build INSTALL_DIR)
 set(UVM_SYSTEMC_ROOT ${INSTALL_DIR})
